0bef0cc5c021590d604827a884519161daa0be14
Manas Ranjan Maharana <al3197@accionlabs.in>
2020-09-21 14:55:19 +0530

    Update: Added comments and format

--- src/main/java/com/cision/accion/sample/FibonacciSeries.java
+++ src/main/java/com/cision/accion/sample/FibonacciSeries.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 
 public class FibonacciSeries {
 
+	// Extract fabonacci number series as per input limit
 	public List<Integer> fibonacciSeries(int limit) {
 		return Stream.iterate(new int[] { 0, 1 }, s -> new int[] { s[1], s[0] + s[1] }).limit(limit).map(n -> n[0])
 				.collect(toList());
